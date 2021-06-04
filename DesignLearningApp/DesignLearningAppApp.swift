61d435cc4376d6d28249a7168373e4f9d4476c64
David.Tan <55678808+daurelius@users.noreply.github.com>
2021-06-04 17:18:40 +0800

    Initial Commit

---
+++ DesignLearningApp/DesignLearningAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  DesignLearningAppApp.swift
+//  DesignLearningApp
+//
+//  Created by David Tandjung on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignLearningAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
