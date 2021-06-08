571e48135f3729aad1c34ff8d249120824ec4db9
David.Tan <55678808+daurelius@users.noreply.github.com>
2021-06-09 01:24:43 +0800

    Create Learning Path List View

---
+++ DesignLearningApp/LearningPathItem.swift
@@ -0,0 +1,44 @@
+//
+//  LearningPathItem.swift
+//  DesignLearningApp
+//
+//  Created by David Tandjung on 08/06/21.
+//
+
+import SwiftUI
+
+struct LearningPathItem: View {
+    var path = ""
+    var body: some View {
+        ZStack{
+            Image(path)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 344, height: 158)
+            ZStack{
+                Rectangle().frame(maxWidth:.infinity)
+                    .foregroundColor(Color("appSecondaryColor"))
+                VStack(alignment:.leading){
+                    Text(path)
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        
+                    Text("Learn UI/UX Design")
+                        .font(.body)
+                        .fontWeight(.bold)
+                }
+                .frame(maxWidth:.infinity, alignment:.leading)
+                .foregroundColor(.white)
+                .padding(.leading)
+            }
+            .padding(.top, 100)
+        }.frame(width: 344, height: 158)
+        .mask(RoundedRectangle(cornerRadius: 11))
+    }
+}
+
+struct LearningPathItem_Previews: PreviewProvider {
+    static var previews: some View {
+        LearningPathItem(path: "Beginer")
+    }
+}
---
+++ DesignLearningApp/LearningPathList.swift
@@ -0,0 +1,43 @@
+//
+//  LearningPathList.swift
+//  DesignLearningApp
+//
+//  Created by David Tandjung on 08/06/21.
+//
+
+import SwiftUI
+
+struct LearningPathList: View {
+    var paths = ["Beginer", "Intermediate", "Expert"]
+    var body: some View {
+        NavigationView{
+            VStack(alignment:.leading){
+                ForEach(paths.sorted(), id: \.self){path in
+                    LearningPathItem(path: path)
+                }
+                Spacer().frame(height:32)
+                Button(action: {}, label: {
+                    Text("Add new goal").bold()
+                })
+                .frame(maxWidth:343)
+                .padding(.vertical)
+                .background(Color("appPrimaryColor"))
+                .foregroundColor(.white)
+                .mask(RoundedRectangle(cornerRadius: 11))
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading , 18)
+            .padding(.top)
+            .navigationTitle("Learning Path")
+            .navigationBarTitleDisplayMode(.inline)
+            .foregroundColor(Color("appSecondaryColor"))
+        }
+    }
+}
+
+struct LearningPathList_Previews: PreviewProvider {
+    static var previews: some View {
+        LearningPathList()
+    }
+}
