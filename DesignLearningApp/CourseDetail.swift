725cd3bb96ca7128b3070114e9680f74e26aea34
Alamsyah Oktavian <alamsyah1999@icloud.com>
2021-06-14 16:28:11 +0800

    struct update

--- DesignLearningApp/CourseDetail.swift
+++ DesignLearningApp/CourseDetail.swift
@@ -12,61 +12,16 @@ struct CourseDetail: View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading) {
-                    Text("Typography Basics")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("judul"))
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                        .padding(.bottom, 5.0)
-                        
-                    Text("Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS.")
-                        .font(.footnote)
-                        .fontWeight(.regular)
-                        .multilineTextAlignment(.leading)
-                        
                     
-                    Text("Most Contaced Tracker")
-                        .font(.body)
-                        .fontWeight(.regular)
-                        .foregroundColor(Color.gray)
-                        .padding(.top)
+                    title()
                         
-                    
-                    Text("doubleclick.net was prevented from profiling you across 2 websites")
-                        .font(.body)
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 5.0)
-                    
-                    
-                    Image("gambar_typo")
-                        .resizable()
-                        .frame(width: /*@START_MENU_TOKEN@*/360.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/231.0/*@END_MENU_TOKEN@*/)
-                        .cornerRadius(10)
-                        .aspectRatio(contentMode: .fit)
-                        .clipped()
-                    
-                    Text("Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS.")
-                        .font(.footnote)
-                        .fontWeight(.regular)
-                        .padding(.top, 5.0)
+                    subline()
                         
-                    Button(action: {
-                        
-                        print("ASHIAP")
-                    }) {
-                        Text("Take Assesment Quiz")
-                            .font(.body)
-                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                            .frame(width: /*@START_MENU_TOKEN@*/360.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
-                            .background(Color("warna_button"))
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                        
-                    }
-                    .padding(.top)
+                    tracker()
                     
-                        
-                    Spacer()
+                    gambar()
+                    
+                    subline2()
                     
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -81,3 +36,75 @@ struct CourseDetail_Previews: PreviewProvider {
         CourseDetail()
     }
 }
+
+struct title: View {
+    var body: some View {
+        Text("Typography Basics")
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundColor(Color("judul"))
+            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+            .padding(.bottom, 5.0)
+    }
+}
+
+struct subline: View {
+    var body: some View {
+        Text("Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS.")
+            .font(.footnote)
+            .fontWeight(.regular)
+            .multilineTextAlignment(.leading)
+    }
+}
+
+struct tracker: View {
+    var body: some View {
+        Text("Most Contaced Tracker")
+            .font(.body)
+            .fontWeight(.regular)
+            .foregroundColor(Color.gray)
+            .padding(.top)
+            
+        
+        Text("doubleclick.net was prevented from profiling you across 2 websites")
+            .font(.body)
+            .fontWeight(.semibold)
+            .padding(.bottom, 5.0)
+    }
+}
+
+struct gambar: View {
+    var body: some View {
+        Image("gambar_typo")
+            .resizable()
+            .frame(width: /*@START_MENU_TOKEN@*/360.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/231.0/*@END_MENU_TOKEN@*/)
+            .cornerRadius(10)
+            .aspectRatio(contentMode: .fit)
+            .clipped()
+    }
+}
+
+struct subline2: View {
+    var body: some View {
+        Text("Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS.")
+            .font(.footnote)
+            .fontWeight(.regular)
+            .padding(.top, 5.0)
+            
+        Button(action: {
+            
+            print("ASHIAP")
+        }) {
+            Text("Take Assesment Quiz")
+                .font(.body)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .frame(width: /*@START_MENU_TOKEN@*/360.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                .background(Color("warna_button"))
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            
+        }
+        .padding(.top)
+    }
+}
+
