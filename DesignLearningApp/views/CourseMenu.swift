46cdd4d51a644c753bcc0e05cf45045dd9020c40
Davin DJ <davindx88@gmail.com>
2022-08-11 00:43:07 +0700

    Fix Scroll Bug on BG Image

--- DesignLearningApp/views/CourseMenu.swift
+++ DesignLearningApp/views/CourseMenu.swift
@@ -12,8 +12,8 @@ struct CourseMenu: View {
     
     var body: some View {
         ZStack{
-            Background()
             ScrollView{
+                Background(imageName: learningPath.image!)
                 VStack(spacing:5){
                     LevelCourseTitle(difficulty: learningPath.difficulty!.name!)
                     LearningCourseTitle(title: learningPath.name!)
@@ -24,53 +24,20 @@ struct CourseMenu: View {
                     }
                 }
                 .padding(.horizontal, 20)
-                .padding(.top, 200)
             }
         }.navigationBarTitle("Course Menu", displayMode: .automatic)
     }
 }
 
-struct CourseMenu_Previews: PreviewProvider {
-    static var previews: some View {
-        let moc = PersistenceController.preview.container.viewContext
-        let path1 = LearningPaths(context: moc)
-        path1.name = "Learn UI/UX design from scratch"
-        path1.image = "Beginner"
-        path1.desc = "Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS."
-        path1.difficulty = Difficulties(context: moc)
-        path1.difficulty?.name = "Beginner"
-        
-        let course1 = Courses(context: moc)
-        course1.thumbnail = "Asset_CP"
-        course1.title = "Color Palette"
-        course1.desc = "Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS."
-        course1.score = 0
-        course1.isDone = false
-        
-        let course2 = Courses(context: moc)
-        course2.thumbnail = "Asset_Tyh"
-        course2.title = "Typography"
-        course2.desc = "Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS."
-        course2.score = 0
-        course2.isDone = false
-        
-        path1.addToCourses(course1)
-        path1.addToCourses(course2)
-        
-        return NavigationView{
-            CourseMenu(learningPath: path1)
-        }
+struct Background: View{
+    var imageName: String
+    
+    var body: some View{
+        Image(imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
     }
 }
-
-struct Background: View{
-    var body: some View{
-        Image("top_bg")
-            .resizable()
-            .frame(width: 400, height: 265, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .padding(.bottom, 520)
-        }
-    }
 
 
 struct LevelCourseTitle: View{
@@ -121,6 +88,40 @@ struct LessonsTitle: View{
                 .font(.title2)
                 .foregroundColor(Color("Subtitle"))
             Spacer()
+        }
+    }
+}
+
+
+struct CourseMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        let moc = PersistenceController.preview.container.viewContext
+        let path1 = LearningPaths(context: moc)
+        path1.name = "Learn UI/UX design from scratch"
+        path1.image = "Beginner"
+        path1.desc = "Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS."
+        path1.difficulty = Difficulties(context: moc)
+        path1.difficulty?.name = "Beginner"
+        
+        let course1 = Courses(context: moc)
+        course1.thumbnail = "Asset_CP"
+        course1.title = "Color Palette"
+        course1.desc = "Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS."
+        course1.score = 0
+        course1.isDone = false
+        
+        let course2 = Courses(context: moc)
+        course2.thumbnail = "Asset_Tyh"
+        course2.title = "Typography"
+        course2.desc = "Design a custom table cell style for nonstandard table rows. Standard styles are great for use in a variety of common scenarios, but some content or your overall app design may call for a heavily customized table appearance. To learn how to create your own cells, see Customizing Cells in Table View Programming Guide for iOS."
+        course2.score = 0
+        course2.isDone = false
+        
+        path1.addToCourses(course1)
+        path1.addToCourses(course2)
+        
+        return NavigationView{
+            CourseMenu(learningPath: path1)
         }
     }
 }
